c214ac1d8f47fa3f28f1cce011697b0c323b563c
Dachel Hernandez <dachelhernandezmarzo@gmail.com>
2026-09-08 15:26:24 +0200

    Refactorización estructural del repositorio y setup del Reto 1

---
+++ 03_Retos_Integracion_JSON/Reto_1_RRHH.py
@@ -0,0 +1,19 @@
+# Reto de Integración 1/6: Filtro de Candidatos (RRHH)
+
+candidatos = [
+    {"nombre": "Juan", "python": True, "salario_esperado": 35000},
+    {"nombre": "Ana", "python": True, "salario_esperado": 28000},
+    {"nombre": "Luis", "python": False, "salario_esperado": 25000},
+    {"nombre": "Marta", "python": True, "salario_esperado": 30000},
+    {"nombre": "Pedro", "python": True, "salario_esperado": 40000}
+]
+
+# 1. Crea la nueva lista vacía para guardar a los candidatos ganadores
+candidatos_viables = []
+
+# 2. Inicia tu bucle para recorrer la lista de candidatos
+# Escribe tu lógica aquí...
+
+
+# 3. Imprime la lista final
+print(candidatos_viables)
